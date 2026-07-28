debd5335eb43fd56628dd4da3139288411009461
mwci98 <mwci98@gmail.com>
2026-07-28 21:01:23 +0530

    Use supplied QPOS artwork for PWA icons

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,8 @@
-const CACHE_VERSION = 'qpos-v5';
+const CACHE_VERSION = 'qpos-v6';
 const APP_SHELL = [
   '/',
   '/index.html',
   '/manifest.webmanifest',
-  '/icons/icon.svg',
   '/icons/icon-192.png',
   '/icons/icon-512.png',
   '/icons/icon-maskable-512.png',
